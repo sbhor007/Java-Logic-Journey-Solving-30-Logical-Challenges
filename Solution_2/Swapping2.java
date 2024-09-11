5d38349ad265a78c731b9540675b0bdb4117b5b7
Santosh Bhor <125242679+sbhor007@users.noreply.github.com>
2024-09-11 05:32:19 +0530

    Swapping Two numbers without using third variable

---
+++ Solution_2/Swapping2.java
@@ -0,0 +1,11 @@
+//// Swaping two numbers without using third variable
+package Solution_2;
+
+public class Swapping2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
